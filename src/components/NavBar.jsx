287afd77d312fc5bf2c60daead05d5f74f6e4ba2
Steve Rector <steverector27@gmail.com>
2024-03-25 15:13:47 -0700

    Add simple home icon

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,12 @@
 import Wrapper from '../assets/wrappers/Navbar';
+import { FaHome } from 'react-icons/fa';
 
 const NavBar = () => {
-  return <Wrapper>Nav Bar</Wrapper>;
+  return (
+    <Wrapper>
+      <h2>Nav Bar</h2>
+      <FaHome />
+    </Wrapper>
+  );
 };
 export default NavBar;
