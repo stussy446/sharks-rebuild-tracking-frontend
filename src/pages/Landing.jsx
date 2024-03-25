ef916fa6132700585f30a3c98491b7b61442e9e3
Steve Rector <steverector27@gmail.com>
2024-03-25 11:53:53 -0700

    Setup landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,22 +1,35 @@
-import styled from 'styled-components';
+import Wrapper from '../assets/wrappers/LandingPage';
+import main from '../../src/assets/images/main.svg';
+import logo from '../../src/assets/images/logo.svg';
+import { Link } from 'react-router-dom';
 
 const Landing = () => {
   return (
     <Wrapper>
-      <h1>Landing Page</h1>
-      <div className="content">some content</div>
+      <nav>
+        <img src={logo} alt="jobify" className="logo" />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span> app
+          </h1>
+          <p>
+            placeholder placeholderplaceholderplaceholderplaceholderplaceholder
+            placeholder placeholder placeholder placeholder placeholder
+            placeholder placeholder placeholder placeholder placeholder
+          </p>
+          <Link to="/regiter" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" />
+      </div>
     </Wrapper>
   );
 };
 
-const Wrapper = styled.div`
-  background: red;
-  h1 {
-    color: white;
-  }
-  .content {
-    background: blue;
-    color: yellow;
-  }
-`;
 export default Landing;
