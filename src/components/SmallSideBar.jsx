819a96b420f40b150ce98d9e6b05bcf98dd12add
Steve Rector <steverector27@gmail.com>
2024-03-26 12:01:31 -0700

    Add small sidebar logic and navigation

--- src/components/SmallSideBar.jsx
+++ src/components/SmallSideBar.jsx
@@ -1,9 +1,30 @@
+import { FaTimes } from 'react-icons/fa';
 import Wrapper from '../assets/wrappers/SmallSidebar';
 import { useDashboardContext } from '../pages/DashboardLayout';
+import Logo from './Logo';
+import Navlinks from './NavLinks';
 
 const SmallSideBar = () => {
-  const data = useDashboardContext();
-  console.log(data);
-  return <Wrapper>Small SideBar </Wrapper>;
+  const { showSidebar, toggleSidebar } = useDashboardContext();
+
+  return (
+    <Wrapper>
+      <div
+        className={
+          showSidebar ? 'sidebar-container-show-sidebar' : 'sidebar-container'
+        }
+      >
+        <div className="content">
+          <button type="button" className="close-btn" onClick={toggleSidebar}>
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <Navlinks />
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 export default SmallSideBar;
